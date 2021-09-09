dcde2b56b489e166d880e45ea8dec134c7933ad8
BasharDamen <bashar.alda@gmail.com>
2021-09-09 13:03:10 +0300

    final result

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,7 +16,7 @@ class Movie extends React.Component {
 
   displayMovie = () => {
     let movieName = this.props.movieName;
-    let movieURL = `https://city-explorer-bd-server.herokuapp.com/movie?query=${movieName}`;
+    let movieURL = `https://city-explorer-bd-server.herokuapp.com/movie?origin_country=${movieName}`;
 
     axios
       .get(movieURL)
@@ -37,20 +39,46 @@ class Movie extends React.Component {
         >
           Related Movies
         </Button>{" "}
+        <Row xs={1} md={3} className="g-4">
         {this.state.moviesArray.map((item, i) => {
           return (
-            <Card className="bg-dark text-white">
-              <Card.Img src={item.imageURL} alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title>Title: {item.title}</Card.Title>
-                <Card.Text>overview: {item.overview}</Card.Text>
-                <Card.Text>Average Votes: {item.avgVotes}</Card.Text>
-                <Card.Text>popularity: {item.popularity}</Card.Text>
-                <Card.Text>released:{item.released}</Card.Text>
-              </Card.ImgOverlay>
-            </Card>
+            <Col>
+              <Card>
+                <Card.Img variant="top" src={item.imageURL} />
+                <Card.Body>
+                  <Card.Title>Title: {item.title}</Card.Title>
+                  <Card.Text>overview: {item.overview}</Card.Text>
+                  <Card.Text>Average Votes: {item.avgVotes}</Card.Text>
+                  <Card.Text>popularity: {item.popularity}</Card.Text>
+                  <Card.Text>released:{item.released}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            // <Card className="bg-dark text-white">
+            //   <Card.Img src={item.imageURL} alt="Card image" />
+            //   <Card.ImgOverlay>
+            //     <Card.Title>Title: {item.title}</Card.Title>
+            //     <Card.Text>overview: {item.overview}</Card.Text>
+            //     <Card.Text>Average Votes: {item.avgVotes}</Card.Text>
+            //     <Card.Text>popularity: {item.popularity}</Card.Text>
+            //     <Card.Text>released:{item.released}</Card.Text>
+            //   </Card.ImgOverlay>
+            // </Card>
           );
         })}
+        </Row>
+        {/* <Col>
+          <Card>
+            <Card.Img variant="top" src={item.imageURL} />
+            <Card.Body>
+              <Card.Title>Title: {item.title}</Card.Title>
+              <Card.Text>overview: {item.overview}</Card.Text>
+              <Card.Text>Average Votes: {item.avgVotes}</Card.Text>
+                <Card.Text>popularity: {item.popularity}</Card.Text>
+                <Card.Text>released:{item.released}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col> */}
       </>
     );
   }
